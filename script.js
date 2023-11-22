55ead89770f8a72ff13e2d7e0d7cc1dfc8ff8713
nilaxsuvisinth <nilaxsuvisinth27@gmail.com>
2023-11-22 12:04:08 +0530

    change some new updates

--- script.js
+++ script.js
@@ -24,15 +24,15 @@ const body = document.querySelector("body"),
       });
 
 // js code to toggle search box
-        searchToggle.addEventListener("click" , () =>{
-        searchToggle.classList.toggle("active");
-      });
+      //   searchToggle.addEventListener("click" , () =>{
+      //   searchToggle.classList.toggle("active");
+      // });
  
       
 //   js code to toggle sidebar
-sidebarOpen.addEventListener("click" , () =>{
-    nav.classList.add("active");
-});
+// sidebarOpen.addEventListener("click" , () =>{
+//     nav.classList.add("active");
+// });
 
 body.addEventListener("click" , e =>{
     let clickedElm = e.target;
@@ -46,48 +46,79 @@ body.addEventListener("click" , e =>{
 //  ===========   image gallary   ============== 
 
 
-$('.products_container').imagesLoaded(function(){
-  $('.products_container').isotope({
-    itemSelector: '.products_card',
-    layoutMode: 'fitRows'
+// ('.products_container').imagesLoaded(function(){
+//   ('.products_container').isotope({
+//     itemSelector: '.products_card',
+//     layoutMode: 'fitRows'
+//   });
+// });
+
+
+// ('.btn.all').click(function(){
+//   ('.products_container').isotope({
+//     filter: '*'
+//   })			
+// });
+
+// ('.btn.featured').click(function(){
+//   ('.products_container').isotope({
+//     filter: '.featured'
+//   })
+// });
+
+// ('.btn.bestseller').click(function(){
+//   ('.products_container').isotope({
+//     filter: '.bestseller'
+//   })
+// });
+
+// ('.btn.latest').click(function(){
+//   ('.products_container').isotope({
+//     filter: '.latest'
+//   })
+// });
+
+
+// ('.btn').click(function(){
+//   ('.btn').removeClass('on');
+//   (this).toggleClass('on');
+// });
+
+
+// ===========================  js for image gallery ===================================
+
+
+$(document).ready(function(){
+
+  $(".filter-button").click(function(){
+      var value = $(this).attr('data-filter');
+      
+      if(value == "all")
+      {
+          //$('.filter').removeClass('hidden');
+          $('.filter').show('1000');
+      }
+      else
+      {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+          $(".filter").not('.'+value).hide('3000');
+          $('.filter').filter('.'+value).show('3000');
+          
+      }
   });
+  
+  if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
 });
 
-
-$('.btn.all').click(function(){
-  $('.products_container').isotope({
-    filter: '*'
-  })			
-});
-
-$('.btn.featured').click(function(){
-  $('.products_container').isotope({
-    filter: '.featured'
-  })
-});
-
-$('.btn.bestseller').click(function(){
-  $('.products_container').isotope({
-    filter: '.bestseller'
-  })
-});
-
-$('.btn.latest').click(function(){
-  $('.products_container').isotope({
-    filter: '.latest'
-  })
-});
+// =======================   image gallery end ===================================
 
 
-$('.btn').click(function(){
-  $('.btn').removeClass('on');
-  $(this).toggleClass('on');
-});
-
-
-
-
-// map
+// =========================== map start ==================================
 function initMap(){
   var options= {
     zoom:20,
@@ -96,3 +127,4 @@ function initMap(){
   var googlemap = new google.maps.Map(document.getElementById("map"), options);
 }
 
+// =========================== map end ==================================
