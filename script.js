9479394782f7eb71c2b845f3f650d612feb8a302
nilaxsuvisinth <nilaxsuvisinth27@gmail.com>
2023-11-24 10:30:02 +0530

    push after viva

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 const body = document.querySelector("body"),
       nav = document.querySelector("nav"),
-      modeToggle = document.querySelector(".dark-light"),
-      searchToggle = document.querySelector(".searchToggle"),
-      sidebarOpen = document.querySelector(".sidebarOpen"),
-      siderbarClose = document.querySelector(".siderbarClose");
+      modeToggle = document.querySelector(".dark-light");
+ 
 
-      let getMode = localStorage.getItem("mode");
-          if(getMode && getMode === "dark-mode"){
-            body.classList.add("dark");
-          }
+        // let getMode = localStorage.getItem("mode");
+        //     if(getMode && getMode === "dark-mode"){
+        //       body.classList.add("dark");
+        //     }
 
 // js code to toggle dark and light mode
       modeToggle.addEventListener("click" , () =>{
@@ -23,24 +21,15 @@ const body = document.querySelector("body"),
         }
       });
 
-// js code to toggle search box
-      //   searchToggle.addEventListener("click" , () =>{
-      //   searchToggle.classList.toggle("active");
-      // });
  
-      
-//   js code to toggle sidebar
-// sidebarOpen.addEventListener("click" , () =>{
-//     nav.classList.add("active");
+
+// body.addEventListener("click" , e =>{
+//     let clickedElm = e.target;
+
+//     if(!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")){
+//         nav.classList.remove("active");
+//     }
 // });
-
-body.addEventListener("click" , e =>{
-    let clickedElm = e.target;
-
-    if(!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")){
-        nav.classList.remove("active");
-    }
-});
 
 
 //  ===========   image gallary   ============== 
